8f78e1a3ecf02d9e5145b6eb5f60f6e7c8c05cd4
MianHannan <hannanchudri@gmail.com>
2021-10-06 21:59:05 +0500

    Initial App

---
+++ src/components/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Expense Tracker Typescript By Mian Hannan</h1>
+    </div>
+  );
+}
+
+export default App;
